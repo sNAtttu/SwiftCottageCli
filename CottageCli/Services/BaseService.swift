3f62198414f46317a911e720f9abff8a8fa0808d
Santeri Nousiainen <snattu91@gmail.com>
2019-10-29 21:41:30 +0200

    Cli parser

---
+++ CottageCli/Services/BaseService.swift
@@ -0,0 +1,25 @@
+//
+//  BaseService.swift
+//  CottageCli
+//
+//  Created by Santeri Nousiainen on 29.10.2019.
+//  Copyright © 2019 Santeri Nousiainen. All rights reserved.
+//
+
+import Foundation
+
+class BaseService {
+    
+    let command: String
+    let action: String
+    
+    init(_ command: String, _ action: String) {
+        self.command = command
+        self.action = action
+    }
+    
+    func printCommand() {
+        print(self.command)
+    }
+    
+}
