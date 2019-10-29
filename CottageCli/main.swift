c5d974c746ed1fe993fa70adc21a14c4fbbba192
Santeri Nousiainen <snattu91@gmail.com>
2019-10-29 20:28:51 +0200

    Initial Commit

---
+++ CottageCli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CottageCli
+//
+//  Created by Santeri Nousiainen on 29.10.2019.
+//  Copyright © 2019 Santeri Nousiainen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
